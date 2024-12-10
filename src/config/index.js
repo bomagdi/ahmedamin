04829371652d7415824e579e192987a602867130
bomagdi <samyy.ads1@gmail.com>
2024-12-10 17:53:26 +0300

    deploy the website

---
+++ src/config/index.js
@@ -0,0 +1,52 @@
+/* eslint-disable max-len */
+import featuredProjects from './featured';
+import projects from './projects';
+
+module.exports = {
+  email: 'aboeyad33550@gmail.com',
+  featuredProjects,
+  projects,
+  skills: ['Smacc', 'Daftra', 'Qoyod', 'GAZT Portal', 'Excel'],
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/jrgarciadev',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/jrgarciadev/',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/jrgarciadev',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/jrgarciadev',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Blog',
+      url: 'https://blog.jrgarciadev.com',
+    },
+    {
+      name: 'Work',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+};
